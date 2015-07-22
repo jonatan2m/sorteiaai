de1692d089670cbe1f7d332aa38002f02fe50eab
Jonatan Machado <jonatan.mac@gmail.com>
2015-07-22 07:17:09 -0300

    Ajustes na interface do resultado de sorteios tipo número

--- js/controllers/numberResultController.js
+++ js/controllers/numberResultController.js
@@ -9,15 +9,31 @@ app.controller('NumberResultController', ['$routeParams',
         result.alias = '';
         result.config = {};
         result.numbers = [];
+        result.configInfo = "";
 
         numberDataService.get($routeParams.id)
         .done(function (data){
             result.id = data.id;
-            result.created = new Date(data.created);
+            
+            result.created = data.created;
             result.alias = data.alias;
             var raffleData = JSON.parse(data.data);
 
             result.config = raffleData.config;
+
+            result.configInfo = "De {begin} até {end},  {{repeat}} repetição.";
+
+            for (var key in result.config){
+                result.configInfo = result.configInfo.replace("{" + key + "}",
+                    result.config[key]);
+            }
+
+            result.configInfo = result.configInfo.replace("{" + 
+            result.config.repeat + "}",
+                    result.config.repeat? "com" : "sem");
+
+
+            
             for (var i = 0; raffleData.result.length > i; i++) {
                 result.numbers.push(raffleData.result[i]);
             }            
